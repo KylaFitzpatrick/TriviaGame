a92e165cea9b765fd535d962777044769c00839c
fitzpaky <kfitzpatrick44@gmail.com>
2020-02-23 15:23:31 -0800

    adding questions and answers in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,13 @@
-var movies = [ 
+var movieQuestions = [ 
+    "What was the title of the movie about Pooh?", 
+    "Which movie was released on June 24, 1994?", 
+    "Which movie was release on July 28, 1951?", 
+    "Which movie was about a boy who didn't want to grow up?", 
+    "In which movie was a deer friends with a rabbit named Thumper?", 
+    "Which movie was about an elephant that could fly?", 
+    "What was the name of the wooden boy whose nose would grow after telling a lie?", 
+    "What was the title of the movie that featured a dwarf named Happy?"];
+var answers = [
     "Winnie the Pooh", 
     "The Lion King", 
     "Alice in Wonderland", 
@@ -6,7 +15,8 @@ var movies = [
     "Bambi", 
     "Dumbo", 
     "Pinocchio", 
-    "Snow White and the Seven Dwarfs"];
+    "Snow White and the Seven Dwarfs"
+]
 
 //create a variable for time remaining
 var timeRemaining = [];
@@ -18,6 +28,10 @@ var answerChoices = [];
 var answer = "";
 //display image/gif of answer
 var imageAnswer = "";
+
+for(var i = 0; i < movieQuestions.length; i++){
+
+}
 
 
 
