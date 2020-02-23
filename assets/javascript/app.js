e7424ebeffd9ba5eb56ad651f855ae6aa6cb7d95
fitzpaky <kfitzpatrick44@gmail.com>
2020-02-23 14:46:22 -0800

    adding files for trivia game as well as html and js

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+var movies = [ 
+    "Winnie the Pooh", 
+    "The Lion King", 
+    "Alice in Wonderland", 
+    "Peter Pan", 
+    "Bambi", 
+    "Dumbo", 
+    "Pinocchio", 
+    "Snow White and the Seven Dwarfs"];
+
+//create a variable for time remaining
+var timeRemaining = [];
+//create a variable for questions
+var questions = [];
+//create a variable for possible answers
+var answerChoices = [];
+//displays correct or no after user guesses question
+var answer = "";
+//display image/gif of answer
+var imageAnswer = "";
+
+
+
+
